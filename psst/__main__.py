8fbec85023da93d34758f183a200e88431a99f1f
Raphael Ahrens <raphael.ahrens@fit.fraunhofer.de>
2016-07-07 12:21:20 +0200

    Changed for a JSON config to a YAML config

--- psst/__main__.py
+++ psst/__main__.py
@@ -4,6 +4,7 @@ import math
 import psst
 import string
 import sys
+import yaml
 
 
 CONFIG = {'choices': string.ascii_letters + string.digits + '!@',
@@ -11,28 +12,30 @@ CONFIG = {'choices': string.ascii_letters + string.digits + '!@',
           }
 
 
-def read_config(config, json_data_file, unsafe):
+def read_config(config, data_file, unsafe):
     """ Read the config for bob
 
     Args:
         config -- the configuration object whith the default values
-        json_data_file -- the json file which holds the configuration inforation
+        data_file -- the YAML file which holds the configuration inforation
     Returns:
         the updated configuration
     """
-    with json_data_file:
-        loaded_conf = json.load(json_data_file)
+    with data_file:
+        loaded_conf = yaml.load(data_file)
         config.update(loaded_conf["psst"])
         length = config['length']
         if not (isinstance(length, int) and length > 0):
             raise ValueError('length needs to be a positive integer')
         choices = config['choices']
         if not (isinstance(choices, str) and len(choices) > 1):
-            raise ValueError('choices should be a string of atleast 32 characters')
-        
+            raise ValueError('choices should be a string of at least 32 characters')
+
         entropy = math.log2(len(choices)) * length
         if not unsafe and entropy < 64:
-            raise ValueError('Current password entropy is {} bits. \nThe entropy of your passwords should be above 64 bits for your own sake.\n If the low entropy is desired add "--unsafe"'.format(entropy))
+            raise ValueError('''Current password entropy is {} bits.
+                                The entropy of your passwords should be above 64 bits
+                                If the low entropy is desired add "--unsafe"'''.format(entropy))
 
         return config
 
